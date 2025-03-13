0a18794bf386269de728aa4153b1e11f59425633
anasabina-rudnic <anasabina.rudnic@gmail.com>
2025-03-13 16:42:34 +0200

    commit

--- Seminar2_CleanCode/src/Main/Main.java
+++ Seminar2_CleanCode/src/Main/Main.java
@@ -1,11 +1,14 @@
 package Main;
 
+import Clase.Angajat;
 import Clase.Aplicant;
 import Readers.AngajatiReader;
 import Readers.BaseReader;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
