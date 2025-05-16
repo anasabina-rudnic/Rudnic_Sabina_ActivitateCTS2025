ad3cc9078005b0f719c1a708d57e6d4e034f6251
anasabina-rudnic <anasabina.rudnic@gmail.com>
2025-05-16 15:29:49 +0300

    commit

---
+++ Seminar10_DP_ChainOfResponsability/src_PF12/main/Main.java
@@ -0,0 +1,21 @@
+package main;
+
+import clase.*;
+
+public class Main {
+    public static void main(String[] args) {
+        AInfoTransport troleibuz = new InfoTroleibuz();
+        AInfoTransport autobuz = new InfoAutobuz();
+        AInfoTransport tramvai = new InfoTramvai();
+        AInfoTransport metrou = new InfoMetrou();
+
+        troleibuz.setNext(autobuz);
+        troleibuz.setNext(tramvai);
+        troleibuz.setNext(metrou);
+
+        troleibuz.recomandaTransport(2);
+        troleibuz.recomandaTransport(12);
+        troleibuz.recomandaTransport(7);
+        troleibuz.recomandaTransport(4);
+    }
+}
